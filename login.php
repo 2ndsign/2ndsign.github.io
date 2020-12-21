226e727ff154ec2b5f53ee4a56afdb3667b4d040
2ndsign <74519952+2ndsign@users.noreply.github.com>
2020-12-21 11:36:43 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<?php
+
+$userList = array(
+    array( "u" => "jack", "p" => "jack123"),
+    array( "u" => "jane", "p" => "jane123")
+    );
+    $user = $_POST [ 'user'];
+    $password = $_POST [ 'password '] ;
+    $isLoggedIn = FALSE;
+    foreach($userList as $rec) {
+        if ($user == $rec['u'] && $password == $rec['p']) {
+        $isLoggedIn = TRUE;
+    break;
+    }}
+    if ($isLoggedIn) {
+    echo "Login SUCCESS";
+    } else {
+    echo "Login FAILED";
+    }
+
+?>
+
+</body>
+
+</html>
