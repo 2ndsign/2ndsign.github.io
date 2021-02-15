d34f11651a8fa7aa1c0a9ef58eafe1e3ea06830c
Nutnisa Thongrussamee <design@Designs-MacBook-Pro.local>
2021-02-15 21:56:23 +0700

    add all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container, Jumbotron, Button,
+  Navbar, Nav, Form, FormControl
+} from 'react-bootstrap';
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+import Home from './Home'
+import About from './About'
+import Pricing from './Pricing'
+import Register from './Register'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <img
+              src="/flower.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            /> Flower
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+
+
+
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/pricing">
+            <Pricing />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+
+
+      </Container>
+    </Router>
   );
 }
 
